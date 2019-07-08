9b811f977524eacb719071c0b7cf4d10cb7c0891
Masahiko Sato <sato_m@mediaxis.jp>
2019-07-08 22:24:44 +0900

    Add /popular_items endpoint

--- models/items.go
+++ models/items.go
@@ -48,6 +48,34 @@ func GetAllItems() Items {
 	return items
 }
 
+func GetPopularItems() Items {
+	titles := []string{
+		"DOPE",
+		"LUCIFER",
+		"バッド・ブラッド悲しみのマフィア",
+		"サバイバー",
+		"オレンジイズニューブラック",
+		"ストレンジャーシングス",
+		"DARK",
+		"ブラック・ミラー",
+		"シャフト",
+		"ジェシカジョーンズ",
+		"ブラック・ミラ バンダースナッチ",
+		"WHATIF 選択の連鎖",
+	}
+	var items Items
+	for i := range titles {
+		id := i + 1
+		title := titles[i]
+		imageName := strconv.Itoa((i % 12) + 1) + ".jpg"
+		imageUrl := "http://localhost:8000/popular/" + imageName
+		item := Item{Id:id, Title: title, ImageUrl: imageUrl}
+		items = append(items, item)
+	}
+
+	return items
+}
+
 func (i Items) Filter(title string) Items {
 	items := Items{}
 
--- server.go
+++ server.go
@@ -17,12 +17,25 @@ func getItems(w http.ResponseWriter, r *http.Request)  {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 
-	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.Write(res)
+}
+
+func getPopularItems(w http.ResponseWriter, r *http.Request)  {
+
+	res,err := json.Marshal(models.GetPopularItems())
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.Write(res)
 }
 
 func main() {
 	http.HandleFunc("/items", getItems)
+	http.HandleFunc("/popular_items", getPopularItems)
 	http.Handle("/", http.FileServer(http.Dir("assets/images")))
 
 	err := http.ListenAndServe(":8000", nil)
